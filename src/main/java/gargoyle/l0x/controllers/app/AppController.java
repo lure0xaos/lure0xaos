901d48dcf291ad379677b47c4fa9ee583dbd5c90
Андрей Крюковский <akrukovskiy@colvir.ru>
2019-07-23 17:27:12 +0300

    comments

--- src/main/java/gargoyle/l0x/controllers/app/AppController.java
+++ src/main/java/gargoyle/l0x/controllers/app/AppController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 import static gargoyle.l0x.controllers.admin.AdminController.MODEL_ITEMS;
@@ -25,6 +26,7 @@ public class AppController {
     public static final String VIEW_INDEX = "app/index";
     public static final String VIEW_CREATION = "app/creation";
     public static final String VIEW_CREATIONS = "app/creations";
+    public static final String VIEW_CREATION_COMMENTS = "app/creation/comments";
     public static final String MODEL_COMMENTS = "comments";
     public static final String MODEL_CREATION = "creation";
     public static final String MODEL_NEWS = "news";
@@ -61,6 +63,20 @@ public class AppController {
     public String commentCreation(@PathVariable("alias") String alias, @RequestParam("head") String head, @RequestParam("source") String source) {
         auth.getLoggedUser().ifPresent(userDto -> app.comment(alias, userDto.getUsername(), head, source));
         return "ok";
+    }
+
+    @RequestMapping(value = "/{alias:[-_a-z0-9]+}/comments/"/*, consumes = MediaType.APPLICATION_JSON_VALUE*/)
+    @ResponseBody
+    public ModelAndView commentCreationComments(@PathVariable("alias") String alias) {
+        return app.findCreation(alias)
+                .map(creation -> new ModelAndView(VIEW_CREATION_COMMENTS, Map.of(
+                        MODEL_COMMENTS, commentConverter.toDto(app.getComments(creation)),
+                        MODEL_AUTH, auth.isFullyLogged()
+                )))
+                .orElseGet(() -> (new ModelAndView(VIEW_CREATION_COMMENTS, Map.of(
+                        MODEL_COMMENTS, List.of(),
+                        MODEL_AUTH, auth.isFullyLogged()
+                ))));
     }
 
     @GetMapping(WebMvcConfig.PATH_ROOT)
